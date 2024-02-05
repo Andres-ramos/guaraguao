4b5816c0ceeae48481af054e47b964a4dc6a6569
Andres-ramos <ramosandres443@gmail.com>
2024-02-05 19:23:35 -0400

    wip.

--- sentinel2.py
+++ sentinel2.py
@@ -1,14 +1,20 @@
-from .earth_engine import earth_engine
+from earth_engine import earth_engine
 import rioxarray as rxr
 from io import BytesIO
 import json
 
 from typing import List
-from typing import json
+# from typing import json
+from typing_json import json
+
+from storage_api import storage_api
+
+
 class Sentinel2:
 
     def __init__(self):
-        self.storage = ""
+        self.storage = storage_api.FileSystemStorage()
+        self.storage.fetch()
         self.data_downloader = earth_engine.EarthEngineAPI()
 
 
@@ -26,19 +32,21 @@ class Sentinel2:
         
         #TODO: Check storage
         in_storage = False  #Hardcoded
-        
-        if in_storage:
-            #TODO: Implement
-            # image = self.storage.get()
-            return 
-        else :
-            try :
-                #TODO: Add storage
-                image_bytes = self.data_downloader.fetch_image_bytes(
-                    aoi, date, bands_list
-                )
-                # image_id = self.storage.put(aoi, date, band_list, image_bytes)
-                byte_stream = BytesIO(image_bytes)
-                return rxr.open_rasterio(byte_stream)
-            except Exception as e:
-                raise e
+        print(self.storage)
+        # if in_storage:
+        #     #TODO: Implement
+        #     # image = self.storage.get()
+        #     return 
+        # else :
+        #     try :
+        #         #TODO: Add storage
+        #         image_bytes = self.data_downloader.fetch_image_bytes(
+        #             aoi, date, bands_list
+        #         )
+        #         # image_id = self.storage.put(aoi, date, band_list, image_bytes)
+        #         byte_stream = BytesIO(image_bytes)
+        #         return rxr.open_rasterio(byte_stream)
+        #     except Exception as e:
+        #         raise e
+
+s = Sentinel2()
--- storage_api/storage_api.py
+++ storage_api/storage_api.py
@@ -4,28 +4,45 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.event import listen
 
-import sqlite3
+from .mapping import SatelliteImage
+
+import pysqlite3 as sqlite3
+
+# import sqlite3
 
 class FileSystemStorage:
     def __init__(self):
         self.path = "./cache"
-        self.conn = self.initialize()  
+        self.conn = self.initialize() 
+        print(SatelliteImage) 
 
     def fetch(self):
+        print("hola")
         return 
     
     def put(self, image_bytes):
-        
+        #Put image in file system cache
+        #Put entry in the database 
         return 
     
     def in_storage(self):
+        #Check db to see if file there
+        #Check in terms of the bands, date and aoi
         return 
     
     def initialize(self):
-        engine = create_engine("sqlite:///gis.db", echo=True)
+        #Create connection to db
+        #Check if tables are created
+        #Otherwies creates them
+        #Returns connection
+        conn = sqlite3.connect('gis.db')
+        conn.enable_load_extension(True)
+        conn.load_extension("mod_spatialite")
 
-        listen(engine, "connect", load_spatialite)
+        # engine = create_engine("sqlite:///gis.db", echo=True)
 
-        conn = engine.connect('gis.db')
+        # listen(engine, "connect", load_spatialite)
+
+        # conn = engine.connect()
 
         return conn
