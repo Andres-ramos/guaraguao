f3357dd33766703fe09744af1baad724d66781de
Andres-ramos <ramosandres443@gmail.com>
2025-05-21 23:16:08 -0400

    Band data fix

--- guaraguao/sentinel2.py
+++ guaraguao/sentinel2.py
@@ -37,7 +37,7 @@ class Sentinel2:
             aoi: json, 
             date: str, #que foking formato?????? 
             band_list: List[str] = ['B1','B2', 'B3', 'B4', 'B5', 'B6', 'B7',
-             'B8', 'B8A','B9', 'B11' ,'B12']
+             'B8', 'B8A','B9', 'B10' ,'B11' ,'B12']
         ) -> xarray.Dataset:
         """
         Fetches image from data source or from cache
